7721dd22b17171de5c648128840cf2e4fb2478fa
Hitesh Bera <hiteshbera1332@gmail.com>
2026-07-10 16:35:20 +0530

    Time: 75 ms (33.84%), Space: 82.5 MB (46.44%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>>ans;
+    vector<int>cur;
+    void f(int st,int n,int k){
+        if(cur.size()==k){
+            ans.push_back(cur);
+            return;
+        }
+        for(int i = st;i<=n;i++){
+            cur.push_back(i);
+
+            f(i+1,n,k);
+
+            cur.pop_back();
+        }
+        return ;
+    }
+    vector<vector<int>> combine(int n, int k) {
+        f(1,n,k);
+        return ans;
+    }
+};
