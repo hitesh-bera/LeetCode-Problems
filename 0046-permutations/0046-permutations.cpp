1e09ddd6e5e420a0c92ae3e5d7afa40ccbdf0558
Hitesh Bera <hiteshbera1332@gmail.com>
2026-07-10 14:46:55 +0530

    Time: 3 ms (26.08%), Space: 10.8 MB (35.49%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,20 +1,44 @@
 class Solution {
 public:
-    vector<vector<int>> f(vector<int> arr) {
-        if (arr.size() == 1)
-            return {{arr[0]}};
-        vector<vector<int>> result;
-        for (int i = 0; i < arr.size(); i++) {
-            int num = arr[0];
-            arr.erase(arr.begin() + 0);
-            vector<vector<int>> child_permutations = f(arr);
-            for (auto permutation : child_permutations) {
-                permutation.push_back(num);
-                result.push_back(permutation);
-            }
-            arr.push_back(num);
+    vector<vector<int>> ans; 
+    int n;
+    void f(int i, vector<int>&nums) {
+        if (i == n) {
+            ans.push_back(nums);  
+            return; 
         }
-        return result;
+        for(int j=i;j<n;j++){
+            swap(nums[i],nums[j]);
+
+            f(i+1,nums);
+            swap(nums[i],nums[j]);
+        }
+        return;
     }
-    vector<vector<int>> permute(vector<int>& nums) { return f(nums); }
+    vector<vector<int>> permute(vector<int>& nums) {
+        n = nums.size();
+        f(0,nums);
+        return ans;
+    }
 };
+
+// class Solution {
+// public:
+//     vector<vector<int>> f(vector<int> arr) {
+//         if (arr.size() == 1)
+//             return {{arr[0]}};
+//         vector<vector<int>> result;
+//         for (int i = 0; i < arr.size(); i++) {
+//             int num = arr[0];
+//             arr.erase(arr.begin() + 0);
+//             vector<vector<int>> child_permutations = f(arr);
+//             for (auto permutation : child_permutations) {
+//                 permutation.push_back(num);
+//                 result.push_back(permutation);
+//             }
+//             arr.push_back(num);
+//         }
+//         return result;
+//     }
+//     vector<vector<int>> permute(vector<int>& nums) { return f(nums); }
+// };
