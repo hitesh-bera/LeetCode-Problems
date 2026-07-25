52646ba0c8217b3d1d605eb0164b4cbdecc7a132
Hitesh Bera <hiteshbera1332@gmail.com>
2026-07-25 11:23:36 +0530

    Time: 59 ms (15.95%), Space: 66.4 MB (10.6%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -2,39 +2,49 @@ class Solution {
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<vector<int>>adj(n);
-        vector<int>in(n,0);
-        for(int v=0;v<n;v++){
-            for(int u : graph[v]){
-                adj[u].push_back(v);
-                in[v]++;
+        vector<vector<int>> reversedAdj(n);
+        vector<int> indegree(n, 0);
+
+        // Build reversed graph and set in-degrees
+        for (int v = 0; v < n; v++) {
+            for (int u : graph[v]) {
+                reversedAdj[u].push_back(v); // Reverse edge: u -> v
             }
+            indegree[v] = graph[v].size(); // Terminal nodes will have indegree == 0
         }
 
-        queue<int>q;
-        vector<int>safeNodes;
+        queue<int> q;
+        vector<bool> isSafe(n, false);
 
-        for(int i=0;i<n;i++){
-            if(in[i] == 0){
+        // Push all terminal nodes (indegree == 0 in reversed graph)
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0) {
                 q.push(i);
             }
         }
 
-        while(!q.empty()){
-            int top = q.front();
+        // Process queue (Kahn's algorithm)
+        while (!q.empty()) {
+            int curr = q.front();
             q.pop();
+            isSafe[curr] = true;
 
-            safeNodes.push_back(top);
-
-            for(int neibr : adj[top]){
-                in[neibr]--;
-
-                if(in[neibr] == 0){
-                    q.push(neibr);
+            for (int neighbor : reversedAdj[curr]) {
+                indegree[neighbor]--;
+                if (indegree[neighbor] == 0) {
+                    q.push(neighbor);
                 }
             }
         }
-        sort(safeNodes.begin(),safeNodes.end());
+
+        // Collect safe nodes in ascending order without sorting
+        vector<int> safeNodes;
+        for (int i = 0; i < n; i++) {
+            if (isSafe[i]) {
+                safeNodes.push_back(i);
+            }
+        }
+
         return safeNodes;
     }
 };
