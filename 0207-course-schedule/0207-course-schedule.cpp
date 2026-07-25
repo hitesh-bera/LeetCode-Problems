b48a9579e1dddbdec0a9db00b58b48e74773e9d6
Hitesh Bera <hiteshbera1332@gmail.com>
2026-07-25 10:29:53 +0530

    Time: 7 ms (45.1%), Space: 20.2 MB (24.46%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+        vector<int> indegree(numCourses, 0);
+        for (auto edge : prerequisites) {
+            int u = edge[1];
+            int v = edge[0];
+            adj[u].push_back(v);
+            indegree[v]++;
+        }
+
+        queue<int>q;
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+
+        int topoSize = 0;
+        while(!q.empty()){
+            int top = q.front();
+            q.pop();
+
+            topoSize++;
+            for(int neibr : adj[top]){
+                indegree[neibr]--;
+                if(indegree[neibr] == 0){
+                    q.push(neibr);
+                }
+            }
+        }
+        return numCourses == topoSize;
+    }
+};
