f5878c887a7de65e99a73f8190104eae9a4f468a
Hitesh Bera <hiteshbera1332@gmail.com>
2026-08-07 00:20:27 +0530

    Time: 0 ms (100%), Space: 11.8 MB (95.28%) - LeetHub

--- 1893-check-if-all-the-integers-in-a-range-are-covered/1893-check-if-all-the-integers-in-a-range-are-covered.cpp
+++ 1893-check-if-all-the-integers-in-a-range-are-covered/1893-check-if-all-the-integers-in-a-range-are-covered.cpp
@@ -1,3 +1,5 @@
+/*
+//1. main idea approach
 class Solution {
 public:
     bool isCovered(vector<vector<int>>& ranges, int left, int right) {
@@ -13,6 +15,29 @@ public:
         }
         for(int i = left;i<=right;i++){
             if(diff[i] <= 0)return false;
+        }
+        return true;
+    }
+};
+*/
+
+//2. slightly optimized version
+class Solution {
+public:
+    bool isCovered(vector<vector<int>>& ranges, int left, int right) {
+        vector<int>diff(52,0);
+        for(const auto &range : ranges){
+            int l = range[0];
+            int r = range[1];
+            diff[l] += 1;
+            diff[r + 1] -= 1;
+        }
+
+        int runningIntervals = 0;
+        for(int i = 1;i<= right;i++){
+            runningIntervals += diff[i];
+
+            if(i >= left && runningIntervals <= 0)return false;
         }
         return true;
     }
