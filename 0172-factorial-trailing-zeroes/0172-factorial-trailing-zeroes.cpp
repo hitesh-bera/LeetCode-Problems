9c4161f47591230fdad5098d22f07853daf19bec
Hitesh Bera <hiteshbera1332@gmail.com>
2026-08-09 19:06:11 +0530

    Time: 0 ms (100%), Space: 8.1 MB (50.84%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
@@ -1,3 +1,5 @@
+/*
+//1.
 class Solution {
 public:
     int trailingZeroes(int n) {
@@ -6,7 +8,7 @@ public:
 
         int two = 0;
         int five = 0;
-
+  
         for (int i = 1; i <= n; i++) {
             int num = i;
             while(num%2 == 0){
@@ -19,5 +21,30 @@ public:
             }
         }
         return min(five,two);
+    }
+};
+*/
+
+//2. optimized version
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int five = 0;
+        int two = 0;
+
+        int num = n;
+        int d = 5;
+        while(num/d != 0){
+            five += num/d;
+            d *= 5;
+        }
+
+        num = n;
+        d = 2;
+        while(num/d != 0){
+            two += num/d;
+            d *= 2;
+        }
+        return min(two, five);
     }
 };
